291eeb89a247add246197e00d2e1d5c29fe3a005
Solomon Ochepa <solomonochepa@gmail.com>
2024-05-24 13:55:56 +0100

    Create tasks module

---
+++ modules/Task/app/Http/Controllers/TaskController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Modules\Task\App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('task::index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('task::create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Show the specified resource.
+     */
+    public function show($id)
+    {
+        return view('task::show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        return view('task::edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ modules/Task/resources/views/index.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('task.name') !!}</p>
+</x-app-layout>
