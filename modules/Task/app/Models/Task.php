3ba64f32113748c115232aa71d6b8344ed3fe4b5
Solomon Ochepa <solomonochepa@gmail.com>
2024-05-26 09:55:20 +0100

    Add support for trash to Task

--- modules/Task/app/Models/Task.php
+++ modules/Task/app/Models/Task.php
@@ -5,10 +5,11 @@ namespace Modules\Task\App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
-    use HasFactory, HasUuids;
+    use HasFactory, HasUuids, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
