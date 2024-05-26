444514f92a25cc9d77f26787308b15586f95e979
Solomon Ochepa <solomonochepa@gmail.com>
2024-05-26 10:29:50 +0100

    [format] Update task module

--- modules/Task/app/Models/Task.php
+++ modules/Task/app/Models/Task.php
@@ -3,8 +3,8 @@
 namespace Modules\Task\App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
